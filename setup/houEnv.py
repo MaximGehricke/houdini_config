934a0cd9c653ea5496b14e9de8cb86284fb58c06
68394215+MaximGehricke@users.noreply.github.com <68394215+MaximGehricke@users.noreply.github.com>
2025-07-11 19:42:01 +0100

    install update

--- setup/houEnv.py
+++ setup/houEnv.py
@@ -1,5 +1,8 @@
 #houEnv
 #sets up the Houdini Environment
+import time, os, sys
+
+locations = ["C:/Users/Maxim/Documents/","/net/homes/mgehrick/"]
 
 boilerplate = '''
 # ===============================================================
@@ -28,7 +31,7 @@ HOUDINI_VEX_PATH = "$HOUDINI_REPO/scripts/vex;&"
 
 
 def winOrLinux():
-    import sys
+
     if sys.platform.startswith('win'):
         return "windows"  
     elif sys.platform.startswith('linux'):
@@ -36,15 +39,42 @@ def winOrLinux():
     else:
         return "other"
     
+def findHoudiniEnv(loc):
+    #finds install location of Houdini
+    print("Searching locations: "+str(loc))
+    houdini_folders = []
+    for path in loc:
+        for root, dirs, files in os.walk(path):
+            for dir_name in dirs:
+                # Convert the directory name to lowercase for case-insensitive matching
+                if "houdini" in dir_name.lower():
+                    for root, dirs, files in os.walk(os.path.join(root, dir_name)):
+                        for file in files:
+                            # print(str(file))
+                            if "houdini.env" in file.lower():
+                                full_path = os.path.join(root, dir_name, file)
+                                if not "backup" in full_path.lower():
+                                    print(f"[FOUND]   '{dir_name}' (Full path: {full_path})")
+                                    houdini_folders.append(full_path)
+    if not houdini_folders:
+        houdini_folders = input("Pls enter path to houdini.env : ")
+    return [item.replace("\\", "/").replace("\\","/") for item in houdini_folders]
+        
+
 
 def main():
-    os = winOrLinux()
-    print(os)
-    if os == "windows":
-        output = boilerplate.replace("# windows path: ","")
-    else:
-        output = boilerplate.replace("# linux path: ","")
-    print(output)
+    system = winOrLinux()
+    print(system)
+    
+    print("folder: ", findHoudiniEnv(locations))
+    print("done")
+
+    # if os == "windows":
+    #     output = boilerplate.replace("# windows path: ","")
+    # else:
+    #     output = boilerplate.replace("# linux path: ","")
+    # print(output)
+    #time.sleep(10)
 
 
 

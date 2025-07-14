aebf18ec0f997988732d79f92906f91fd970c883
MaximGehricke <68394215+MaximGehricke@users.noreply.github.com>
2025-07-14 09:59:28 +0100

    Update SL.py
    indentation fixes
    should be final now

--- scripts/python/fsmgh/SL.py
+++ scripts/python/fsmgh/SL.py
@@ -6,19 +6,19 @@
 import hou
 
 def main():
-  node = hou.node('/obj/sl_sequence_manager1')
-  desktop = hou.ui.curDesktop()
-  if node is not None:
-      desktop = hou.ui.curDesktop()
-      fp = desktop.createFloatingPane(hou.paneTabType.Parm)
-      fp.setCurrentNode(node)
-  else:
-      node = hou.node('/obj/sl_sequence_manager')
-      desktop = hou.ui.curDesktop()
-      if node is not None:
-          desktop = hou.ui.curDesktop()
-          fp = desktop.createFloatingPane(hou.paneTabType.Parm)
-          fp.setCurrentNode(node)
+    node = hou.node('/obj/sl_sequence_manager1')
+    desktop = hou.ui.curDesktop()
+    if node is not None:
+        desktop = hou.ui.curDesktop()
+        fp = desktop.createFloatingPane(hou.paneTabType.Parm)
+        fp.setCurrentNode(node)
+    else:
+        node = hou.node('/obj/sl_sequence_manager')
+        desktop = hou.ui.curDesktop()
+        if node is not None:
+            desktop = hou.ui.curDesktop()
+            fp = desktop.createFloatingPane(hou.paneTabType.Parm)
+            fp.setCurrentNode(node)
 
 
 if __name__ == "__main__":
